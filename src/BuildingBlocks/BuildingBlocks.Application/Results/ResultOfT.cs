7414eb79a2b3dbcadae8812338c04453bdae86fe
Abdullah Ibrahim <abdullahibrahim.business@gmail.com>
2026-07-24 04:49:40 +0300

    feat(application): introduce result pattern

---
+++ src/BuildingBlocks/BuildingBlocks.Application/Results/ResultOfT.cs
@@ -0,0 +1,31 @@
+namespace BuildingBlocks.Application.Results;
+
+public sealed class Result<T>
+{
+    private readonly T? _value;
+
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public Error Error { get; }
+    public T Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("Cannot access the value of a failed result.");
+
+    private Result(T value)
+    {
+        IsSuccess = true;
+        Error = Error.None;
+        _value = value;
+    }
+
+    private Result(Error error)
+    {
+        IsSuccess = false;
+        Error = error;
+        _value = default;
+    }
+
+    public static Result<T> Success(T value) => new(value);
+
+    public static Result<T> Failure(Error error) => new(error);
+}
