5fb220c875c0a191c548c89cd0147a6a4f9442b0
Abdullah Ibrahim <abdullahibrahim.business@gmail.com>
2026-07-25 08:13:23 +0300

    fix swagger version issue

--- src/Bank.Server/Bank.Server.Api/Program.cs
+++ src/Bank.Server/Bank.Server.Api/Program.cs
@@ -3,6 +3,7 @@ using Bank.Server.Application.Handlers;
 using Bank.Server.Infrastructure.DependencyInjection;
 using Bank.Server.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,8 +14,13 @@ builder.Services.AddHealthChecks();
 
 // Use the traditional Swashbuckle Swagger generation engine
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    // The UI title/version label; not the OpenAPI version
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -26,9 +32,11 @@ if (app.Environment.IsDevelopment())
         dbContext.Database.Migrate();
     }
 
-    app.UseSwagger();
+    app.UseSwagger(c => c.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi2_0
+);
     app.UseSwaggerUI(options =>
     {
+
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         options.RoutePrefix = "swagger";
     });
